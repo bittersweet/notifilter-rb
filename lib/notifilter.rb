fca5374fd7f0e962a8f95feb50bd6f74908e647d
Mark Mulder <markmulder@gmail.com>
2015-10-09 16:34:14 +0200

    Make host,port configurable

--- lib/notifilter.rb
+++ lib/notifilter.rb
@@ -2,12 +2,19 @@ require 'socket'
 require 'json'
 
 class Notifilter
+  def self.host
+    ENV["NOTIFILTER_HOST"] || "127.0.0.1"
+  end
+  def self.port
+    ENV["NOTIFILTER_PORT"] || 8000
+  end
+
   def self.notify(key, data)
     message = {"identifier" => key, "data" => data}.to_json
 
     begin
       socket = UDPSocket.new
-      socket.send(message, 0, "127.0.0.1", 8000)
+      socket.send(message, 0, host, port)
     rescue Exception => e
       puts "Exception in Notifilter: #{e.message}"
     ensure
